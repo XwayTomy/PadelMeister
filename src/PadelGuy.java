93cfc22da9823eee233fbdf4d9b1c2ddee6a382c
XwayTomy <Tamas_nagy@hotmail.com>
2020-11-20 21:30:01 +0100

    konstruktor

--- src/PadelGuy.java
+++ src/PadelGuy.java
@@ -1,9 +1,12 @@
 public class PadelGuy {
+    boolean harEgnaBollar;
+    boolean harEgetRack;
+    int rateOfSkills;
 
-    public PadelGuy() {
-        boolean harEgnaBollar;
-        boolean harEgetRack;
-        int rateOfSkills;
+    public PadelGuy(boolean egnabollar, boolean egetRack, int skill) {
+       this.harEgnaBollar = egnabollar;
+       this.harEgetRack   = egetRack;
+       this.rateOfSkills  = skill;
 
     }
 }
